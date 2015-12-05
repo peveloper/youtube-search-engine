3d44ae1abbdaecfe245c1184089ef8902621af73
SniPierZz <SniPierZz@snipierzzs-macbook-pro.home>
2015-12-05 16:30:51 +0100

    Backend and frontend
    Created a Server in node.js that uses express + jade for templating.
    to start the server: nom start from terminal or run from webStorm.
    Server listening @ localhost port 6969

---
+++ node/IR_Project/routes/search.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var router = express.Router();
+
+router.get("/", function (req,res,next) {
+    var userQuery = req.query.userQuery;
+    console.log(userQuery);
+    var obj ={};
+    obj.userQuery = userQuery;
+    var results = [{link:"www.youtube.com",text:"this is youtube"},{link:"www.facebook.com",text:"this is facebook"}];
+    var pageNumber = 3;
+    obj.results = results;
+    obj.pageNumber = [1,2,3,4,5];//set the numbers of page
+
+
+    obj.pageNumber[0]="..."
+    res.render('results', obj);
+});
+
+
+router.get("/:userQuery/:page", function (req,res,next) {
+    var userQuery = req.params.userQuery;
+    var page = req.params.page;
+    console.log(userQuery);
+    var obj ={};
+    obj.userQuery = userQuery;
+    var results = [{link:"www.youtube.com",text:"this is youtube"},{link:"www.facebook.com",text:"this is facebook"}];
+    var pageNumber = 3;
+    obj.results = results;
+    obj.pageNumber = [1,2,3,4,5];//set the numbers of page
+
+    obj.pageNumber[page-1]="..."
+    res.render('results', obj);
+});
+
+module.exports = router;
