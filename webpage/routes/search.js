1f5781b3a6a0e59a9b94418b8af906de1fc26aed
SniPierZz <SniPierZz@snipierzzs-macbook-pro.home>
2015-12-13 23:14:24 +0100

    Final Commit
    It works fine

--- webpage/routes/search.js
+++ webpage/routes/search.js
@@ -6,7 +6,15 @@ router.get('/', function(req, res, next) {
   var userQuery = req.query.userQuery;
   var obj = {};
   obj.query = userQuery;
-  obj.topic = "nfl"; //TODO change topic with req.query.topic
+
+  if(req.query.topic!= undefined){
+    obj.topic = req.query.topic;
+  }else{
+    obj.topic="NBA NFL"
+  }
+  if (obj.query==""){
+    obj.query=obj.topic
+  }
   var results = undefined;
   var ws = new WebSocket('ws://localhost:8887');  // per il mio computer 'ws://test.secure.dev:8887'
 
@@ -25,17 +33,26 @@ router.get('/', function(req, res, next) {
     }
     obj.results = results.slice(0,10);
     obj.pageNumber[0] = -1;
+    obj.before =[];
+    obj.after = [];
+
+    obj.after.push(obj.pageNumber[obj.pageNumber.length-1]);
+    obj.pageNumber = obj.pageNumber.slice(0,5);
     res.render('results', obj);
   };
 
 });
 
-router.get('/:userQuery/:page', function(req, res, next) {
+router.get('/:topic/:userQuery/:page', function(req, res, next) {
   var userQuery = req.params.userQuery;
   var page = req.params.page;
   var obj = {};
   obj.query = userQuery;
-  obj.topic = "nfl";  //TODO change topic with req.params.topic
+  if(req.params.topic!= undefined){
+    obj.topic = req.params.topic;
+  }else{
+    obj.topic = "NBA NFL";
+  }
   var results = undefined;
   var ws = new WebSocket('ws://localhost:8887'); // per il mio computer 'ws://test.secure.dev:8887'
 
@@ -54,6 +71,20 @@ router.get('/:userQuery/:page', function(req, res, next) {
     }
     obj.results = results.slice((page-1)*10,page*10);
     obj.pageNumber[page - 1] = -1 * obj.pageNumber[page - 1];
+    obj.before =[1];
+    obj.after = [];
+    obj.after.push(obj.pageNumber[obj.pageNumber.length-1]);
+    var min = page-2;
+    if (min<=1){
+      min = 1;
+    }
+    var max = min+5;
+    if (max>=obj.pageNumber.length){
+      obj.after = [];
+      max = obj.pageNumber.length;
+      min = max-5;
+    }
+    obj.pageNumber = obj.pageNumber.slice(min,max);
     res.render('results', obj);
   };
 });
